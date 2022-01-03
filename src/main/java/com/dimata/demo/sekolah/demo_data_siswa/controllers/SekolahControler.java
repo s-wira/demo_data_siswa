306e1abbe560f2b8eb4d0e0356aa751f254879e7
pius11 <96400519+pius11@users.noreply.github.com>
2022-01-03 10:09:35 +0800

    mengubah package

---
+++ src/main/java/com/dimata/demo/sekolah/demo_data_siswa/controllers/SekolahControler.java
@@ -0,0 +1,48 @@
+package com.dimata.demo.sekolah.demo_data_siswa.controllers.datasekolah;
+import com.dimata.demo.sekolah.demo_data_siswa.core.search.CommonParam;
+import com.dimata.demo.sekolah.demo_data_siswa.forms.DataSekolahForm;
+import com.dimata.demo.sekolah.demo_data_siswa.models.table.DataSekolah;
+import com.dimata.demo.sekolah.demo_data_siswa.services.api.DataSekolahApi;
+import com.dimata.demo.sekolah.demo_data_siswa.services.api.datasekolah.DataSekolahApi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class SekolahControler {
+    @Autowired
+    private DataSekolahApi dataSekolahApi;
+
+    private static final String BASE_URL = "/maintainer/v1";
+
+    @PostMapping(path = BASE_URL + "/data_sekolah", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<DataSekolah> maintainerAddDataSekolah(@RequestBody DataSekolahForm form) {
+        return dataSekolahApi.createDataSeklah(form);
+    }
+
+    @GetMapping(path = BASE_URL + "/data_sekolah")
+    public Flux<DataSekolah> maintainerGetAllDataSekolah(CommonParam param) {
+        return dataSekolahApi.getAllDataSekolah(param);
+    }
+
+    @GetMapping(path = BASE_URL + "/data_sekolah/{id_sekolah}")
+    public Mono<DataSekolah> maintainerGetDataSekolah(@PathVariable("id_sekolah") Long id_sekolah) {
+        return dataSekolhaApi.getDataSekolah(id_sekolah);
+    }
+
+    @PutMapping(path = BASE_URL + "/data_sekolah/{id_sekolah}")
+    public Mono<DataSekolah> maintainerUpdateDataSekolah(@PathVariable("id_sekolah") long id_sekolah, @RequestBody DataSeoklahForm form) {
+        return dataSekolahApi.updateDataSekolah(id_sekolah, form);
+    }
+}
